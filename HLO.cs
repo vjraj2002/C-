84fba65afe79414b75ab339afb3e1bde0aeb27af
vjraj2002 <135831698+vjraj2002@users.noreply.github.com>
2023-06-07 17:24:52 +0530

    Add files via upload

---
+++ HLO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmr2sprprj.Test1
+{
+    class HLO
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
